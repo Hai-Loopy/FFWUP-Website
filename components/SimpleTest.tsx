18a5fa6ce3a7c906d0a052b24eab3b3477de391c
Hai-Loopy <diepchanhai2107@gmail.com>
2025-09-06 04:05:17 -0300

    Create SimpleTest.tsx

---
+++ components/SimpleTest.tsx
@@ -0,0 +1,19 @@
+export default function SimpleTest() {
+  return (
+    <div style={{
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      width: '100%',
+      height: '100px',
+      backgroundColor: 'red',
+      color: 'white',
+      fontSize: '24px',
+      textAlign: 'center',
+      lineHeight: '100px',
+      zIndex: 9999
+    }}>
+      SIMPLE TEST COMPONENT IS WORKING - LOGIN BUTTONS SHOULD BE HERE
+    </div>
+  )
+}
